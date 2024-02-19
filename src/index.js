59f990ef74cd3db2df76207175854e8891d135dc
kasperFlo <20kasperbauefr2@gmail.com>
2024-02-19 16:05:04 -0500

    before starting react implement

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './src/App'; // Assuming you'll eventually have an App component.
+
+const root = ReactDOM.createRoot(document.getElementById('text'));
+root.render(<App />);
