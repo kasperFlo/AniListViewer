ee59aa34e66b892e26f3d0f4ff7d6370f1a81ec0
kasperFlo <20kasperbauefr2@gmail.com>
2024-02-19 19:54:32 -0500

    changed UI with tailwind

--- src/myScript.js
+++ src/myScript.js
@@ -1,6 +1,4 @@
-type="text/babel"
-
-document.addEventListener("DOMContentLoaded",event => {
+document.addEventListener("DOMContentLoaded", event => {
     console.dir(("DOM loaded Console Test"))
 
 
@@ -28,8 +26,8 @@ document.addEventListener("DOMContentLoaded",event => {
           }
         } } } `;
     let RequestVariables = {
-        search: "Fate/Zero",
-        page: 2,
+        search: "Attack on Titan",
+        page: 4,
         perPage: 6
     };
 
@@ -39,47 +37,46 @@ document.addEventListener("DOMContentLoaded",event => {
     });
     console.dir(requestBody)
 
-    xhr.open("POST",url,true);
+    xhr.open("POST", url, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader('Accept', 'application/json');
 
     xhr.send(requestBody);
 
     /*takes API json and Displays it onto page */
-    function pageParse(RawJson){
+    function pageParse(RawJson) {
         let pData = RawJson.data.Page.media;
         let ToDisplay = "";
+        textBox.innerText = "";
 
-        for (const media of pData){
-            const NewNode = document.createElement("div");
-            NewNode.classList.add("bg-red-500");
+        for (const media of pData) {
+            const card = document.createElement('div');
+            card.className = 'card m-2 w-2/5 flex flex-row justify-center grid grid-cols-6';
 
-            ToDisplay += ("Media ID: " + media.id + "\n");
-            ToDisplay += ("Title (Romaji): " + media.title.romaji + "\n");
+                const mediaIdDiv = document.createElement('div');
+                mediaIdDiv.className = 'p-3 col-span-2';
+                mediaIdDiv.textContent = `Media ID: ${media.id}`;
 
+                const titleDiv = document.createElement('div');
+                titleDiv.className = ('p-3 col-span-3');
+                titleDiv.textContent = `Title (Romaji): ${media.title.romaji}`;
+
+            card.appendChild(mediaIdDiv);
+            card.appendChild(titleDiv);
+
+            textBox.appendChild(card);
         }
-
-        textBox.innerText = ToDisplay;
-        console.dir(pData);
     }
-    function card(media,title){
-        const element = (
-        <div>
-            <div>media </div>
-            <div> </div>
-        </div>)
 
-        textBox.render(element);
-    }
 
     /*Handles API json and What to do with it */
-    xhr.onreadystatechange = function (){
-        if(this.readyState === XMLHttpRequest.DONE){
+    xhr.onreadystatechange = function () {
+        if (this.readyState === XMLHttpRequest.DONE) {
             if (this.status == "200") { // if response from server is good
                 APIData = JSON.parse(this.responseText);
                 console.dir(APIData);
 
-                textBox.innerText = JSON.stringify(APIData)
+                // textBox.innerText = JSON.stringify(APIData)
                 pageParse(APIData)
             } else {
                 error = this.statusText;
@@ -89,5 +86,4 @@ document.addEventListener("DOMContentLoaded",event => {
     }
 
 
-
-});
+}); // DOM Content
